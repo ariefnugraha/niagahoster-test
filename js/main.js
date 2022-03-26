c8aa9d179c0572e0880ccd325c0aeb295843a739
arief <muhammadariefnugraha77@gmail.com>
2022-03-26 10:28:03 +0700

    add condition to change navbar based on class

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
 // MAKE STICKY SIDEBAR HAVE MAX WIDTH SAME AS ASIDE
-document.querySelector('aside .sticky').style.maxWidth = document.querySelector('aside').offsetWidth + 'px';
+document.querySelector("aside .sticky").style.maxWidth =
+  document.querySelector("aside").offsetWidth + "px";
 
 // HANDLE NAVBAR & STICKY SIDEBAR WHEN SCROLL
-window.addEventListener('scroll', () => {
-    let scrollPos = window.scrollY;
-    let mainContainerPos = document.querySelector('.main-container').getBoundingClientRect().top;
-    let invitationPos = document.querySelector('.invitation').getBoundingClientRect().top;
+window.addEventListener("scroll", () => {
+  let scrollPos = window.scrollY;
+  let mainContainerPos = document
+    .querySelector(".main-content")
+    .getBoundingClientRect().top;
+  let invitationPos = document
+    .querySelector(".invitation")
+    .getBoundingClientRect().top;
 
+  if (
+    !document.querySelector("header").classList.contains("bg-primary-blue-1")
+  ) {
+    scrollPos > 120
+      ? document.querySelector("header").classList.add("bg-primary-blue-1")
+      : document.querySelector("header").classList.remove("bg-primary-blue-1");
+  }
 
-    console.log(mainContainerPos);
-    scrollPos > 120 ? document.querySelector('header').classList.add('bg-primary-blue-1') : document.querySelector('header').classList.remove('bg-primary-blue-1')
-
-    if (mainContainerPos < 120 && invitationPos > 610) {
-        document.querySelector('aside .sticky').classList.add('scroll');
-        document.querySelector('aside .sticky').classList.remove('absolute');
-    } else if (invitationPos <= 608) {
-        document.querySelector('aside .sticky').classList.remove('scroll');
-        document.querySelector('aside .sticky').classList.add('absolute');
-    } else {
-        document.querySelector('aside .sticky').classList.remove('scroll');
-        document.querySelector('aside .sticky').classList.remove('absolute')
-    }
-
-})
+  if (mainContainerPos < 120 && invitationPos > 610) {
+    document.querySelector("aside .sticky").classList.add("scroll");
+    document.querySelector("aside .sticky").classList.remove("absolute");
+  } else if (invitationPos <= 608) {
+    document.querySelector("aside .sticky").classList.remove("scroll");
+    document.querySelector("aside .sticky").classList.add("absolute");
+  } else {
+    document.querySelector("aside .sticky").classList.remove("scroll");
+    document.querySelector("aside .sticky").classList.remove("absolute");
+  }
+});
 
 // OPEN / CLOSE ACCORDION
-document.querySelectorAll('.accordion .accordion-item').forEach(item => {
-    item.querySelector('button').addEventListener('click', (e) => {
-        let btn = e.target;
-        if (item.querySelector('.accordion-content').classList.contains('show')) {
-            item.querySelectorAll('.accordion-content').forEach(content => content.classList.remove('show'));
-            btn.querySelector('svg').style.transform = 'unset';
-        } else {
-            item.querySelectorAll('.accordion-content').forEach(content => content.classList.add('show'));
-            btn.querySelector('svg').style.transform = 'rotate(180deg)';
-        }
-    })
-})
+document.querySelectorAll(".accordion .accordion-item").forEach((item) => {
+  item.querySelector("button").addEventListener("click", (e) => {
+    let btn = e.target;
+    if (item.querySelector(".accordion-content").classList.contains("show")) {
+      item
+        .querySelectorAll(".accordion-content")
+        .forEach((content) => content.classList.remove("show"));
+      btn.querySelector("svg").style.transform = "unset";
+    } else {
+      item
+        .querySelectorAll(".accordion-content")
+        .forEach((content) => content.classList.add("show"));
+      btn.querySelector("svg").style.transform = "rotate(180deg)";
+    }
+  });
+});
